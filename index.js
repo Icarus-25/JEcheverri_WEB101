c5a5a4015a286daf6b9add62bd629ad4b77fe3c1
Joel Echeverri <s-joel.echeverrimart@lwtech.edu>
2025-11-11 15:08:32 -0800

    Add rsvp Step 1 from week 7

--- index.js
+++ index.js
@@ -21,7 +21,29 @@ const toggleDarkMode = () => {
 //             and tell it to use toggleDarkMode as its callback function
 themeButton.addEventListener("click", toggleDarkMode);
 
-/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+/*** Form Handling ***
+  
+  Purpose:
+  - When the user submits the RSVP form, the name and state they 
+    entered should be added to the list of participants.
+
+  When To Modify:
+  - [ ] Project 6 (REQUIRED FEATURE)
+  - [ ] Project 6 (STRETCH FEATURE) 
+  - [ ] Project 7 (REQUIRED FEATURE)
+  - [ ] Project 9 (REQUIRED FEATURE)
+  - [ ] Any time between / after
+***/
+
+// Step 1: Add your query for the submit RSVP button here
+
+const addParticipant = (event) => {
+    // Step 2: Write your code to manipulate the DOM here
+
+    event.preventDefault();
+}
+
+// Step 3: Add a click event listener to the submit RSVP button here
 /*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
 /*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
 /*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
